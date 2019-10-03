1d502647b7cfb43de6ee2a227e4bae904b1ad1d7
Thiago Almeida <thiago9864@hotmail.com>
2019-10-03 20:18:08 -0300

    Update HeapSort.h

--- src/ordenacao/HeapSort.h
+++ src/ordenacao/HeapSort.h
@@ -3,8 +3,8 @@
     HeapSort.h
     Propósito: Implementação do algoritmo HeapSort.
 
-    @author Luan Reis
-    @version 1.0 21/08/19
+    @author Luan Reis, Renan Nunes
+    @version 2.0 03/10/19
 */
 
 #include <iostream>
@@ -25,28 +25,28 @@ public:
      */
     void ordenar(T *v, int n)
     {
-        constroiHeap(n, v);
-        for (int m = n; m >= 1; --m)
+        constroiHeap(n,v);
+        for (int i = n-1; i > 1; i--)
         {
-            troca2(v, m);
-            peneira(m - 1, v);
+            constroiHeap(i,v);
+            troca2(v,0,i);
         }
-    }
+    };
 
     /**
      * Ordenação para inteiros
      * @param v Vetor de inteiros
      * @param t Tamanho do vetor
      */
-    void ordenarInt(int *v, int n)
+    void ordenarInt(T *v, int n)
     {
-        constroiHeapInt(n, v);
-        for (int m = n; m >= 1; --m)
+        constroiHeapInt(n,v);
+        for (int i = n-1; i > 1; i--)
         {
-            troca2(v, m);
-            peneiraInt(m - 1, v);
+            constroiHeapInt(i,v);
+            troca2(v,0,i);
         }
-    }
+    };
 
     void resetContadores(){
         num_trocas=0;
@@ -65,71 +65,75 @@ private:
     void constroiHeap(int m, T *vet)
     {
 
-        for (int i = 0; i <= m; i++)
+        int tam = m;
+        int p = (int) (tam/2) - 1;
+        bool existe2 = true;
+
+        while(p > -1)
         {
-            int f = i + 1;
-            while (f > 0 && comparador(vet[f / 2], vet[f]))
+            int pai = vet[p];
+            int filho1 = vet[2*p];
+            int filho2 = vet[2*p+1];
+
+            if(2*p+1 >= tam)
             {
-                //num_comparacoes++;
-                troca(vet, f);
-                f /= 2;
+                existe2 = false;
             }
-        }
-        //for (int z=0;z<=m;z++){
-        //    cout<<"construindo a arvore"<<vet[z].id<<endl ;
-        //}
-    }
 
-    void peneira(int m, T *v)
-    {
-        int p = 0, f = 1;
-        T x = v[0];
-        while (f <= m)
-        {
-            if (f <= m && comparador(v[f], v[f + 1]))
-                ++f;
-            if (x.id >= v[f].id)
-                break;
-            v[p] = v[f];
-            p = f, f = 2 * p;
-            num_comparacoes++;
-        }
-        v[p] = x;
-    }
-
-    void constroiHeapInt(int m, int *vet)
-    {
-
-        for (int i = 0; i <= m; i++)
-        {
-            int f = i + 1;
-            while (f > 0 && comparadorInt(vet[f / 2], vet[f]))
+            if(comparador(filho1, filho2) && existe2)
             {
-                //num_comparacoes++;
-                troca(vet, f);
-                f /= 2;
+                if(comparador(pai, filho2))
+                {
+                    troca2(vet,p,2*p+1);
+                }
+            } else
+            {
+                troca2(vet,p,2*p);
             }
+            p--;
         }
-        //for (int z=0;z<=m;z++){
-        //    cout<<"construindo a arvore"<<vet[z].id<<endl ;
-        //}
+        /*
+        for (int z=0;z<=m;z++){
+            cout<<"construindo a arvore"<<vet[z].id<<endl ;
+        }
+        */
     }
 
-    void peneiraInt(int m, int *v)
+    void constroiHeapInt(int m, T *vet)
     {
-        int p = 0, f = 1;
-        T x = v[0];
-        while (f <= m)
+
+        int tam = m;
+        int p = (int) (tam/2) - 1;
+        bool existe2 = true;
+
+        while(p > -1)
         {
-            if (f <= m && comparadorInt(v[f], v[f + 1]))
-                ++f;
-            if (x >= v[f])
-                break;
-            v[p] = v[f];
-            p = f, f = 2 * p;
-            num_comparacoes++;
+            int pai = vet[p];
+            int filho1 = vet[2*p];
+            int filho2 = vet[2*p+1];
+
+            if(2*p+1 >= tam)
+            {
+                existe2 = false;
+            }
+
+            if(comparadorInt(filho1, filho2) && existe2)
+            {
+                if(comparadorInt(pai, filho2))
+                {
+                    troca2(vet,p,2*p+1);
+                }
+            } else
+            {
+                troca2(vet,p,2*p);
+            }
+            p--;
         }
-        v[p] = x;
+        /*
+        for (int z=0;z<=m;z++){
+            cout<<"construindo a arvore"<<vet[z]<<endl ;
+        }
+        */
     }
 
     bool comparador(T a, T b){
@@ -150,12 +154,13 @@ private:
         num_trocas++;
     }
 
-    void troca2(T *vet, int m)
+    void troca2(T *vet, int i, int j)
     {
+
         T aux;
-        aux = vet[0];
-        vet[0] = vet[m];
-        vet[m] = aux;
+        aux = vet[i];
+        vet[i] = vet[j];
+        vet[j] = aux;
         num_trocas++;
     }
 
