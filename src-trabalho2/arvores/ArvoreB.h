ff16baa7b82d25b59f610e24df5ff9d11801cd5d
Luan Ciribelli <luanreisciribelli@ice.ufjf.br>
2019-10-27 11:43:10 -0300

    Create ArvoreB.h

---
+++ src-trabalho2/arvores/ArvoreB.h
@@ -0,0 +1,55 @@
+/**
+    Universidade Federal de Juiz de Fora
+    ArvoreB.h
+    Propósito: Classe para implementar a arvore B
+
+    @author: Luan Reis Ciribelli
+    @version 1.0 27/10/2019
+*/
+
+#ifndef SRC_TRABALHO2_ARVOREB_H
+#define SRC_TRABALHO2_ARVOREB_H
+
+#include <iostream>
+#include "BaseArvores.h"
+#include "No.h"
+
+
+Class ArvoreB
+        {
+
+                public:
+
+            arvoreB(int d,No P){
+
+                No n= new No(P);
+
+
+
+
+            }
+
+                bool busca(No x){
+
+                    return false
+                }
+                void inserir(No a){
+
+
+
+                }
+                void remove(No y){
+
+
+
+                }
+
+
+        private:
+            No n;
+
+
+        };
+
+
+#endif //SRC_TRABALHO2_ARVOREB_H
