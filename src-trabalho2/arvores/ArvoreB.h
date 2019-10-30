badf94f1a627626281db3ae14d30ca207c079480
Luan Ciribelli <44177584+LuanCiribelli@users.noreply.github.com>
2019-10-30 15:38:42 -0300

    Add files via upload

--- src-trabalho2/arvores/ArvoreB.h
+++ src-trabalho2/arvores/ArvoreB.h
@@ -5,6 +5,11 @@
 
     @author: Luan Reis Ciribelli
     @version 1.0 27/10/2019
+
+https://www.geeksforgeeks.org/introduction-of-b-tree-2/
+https://www.geeksforgeeks.org/insert-operation-in-b-tree/
+https://www.geeksforgeeks.org/delete-operation-in-b-tree/
+
 */
 
 #ifndef SRC_TRABALHO2_ARVOREB_H
@@ -14,42 +19,87 @@
 #include "BaseArvores.h"
 #include "No.h"
 
+Class ArvoreB : protected BaseArvores
+{
+private:
+    No raiz;
+ 
+    int t;
+public:
+//Construtor vazio
+  
+   
+   arvoreB(int t){
 
-Class ArvoreB
-        {
+        raiz = NULL;  this.t =t; 
 
-                public:
+   }
 
-            arvoreB(int d,No P){
+    bool buscachave(No x)
+    {
 
-                No n= new No(P);
+        return (raiz == NULL)? NULL : raiz->search(k);
+    }
+
+    void Percorre() 
+    {  if (raiz != NULL) raiz->traverse(); } 
+
+   NoB::NoB(int _t, bool _leaf) 
+{ 
+    // Copy the given minimum degree and leaf property 
+    t = _t; 
+    isLeaf = _leaf; 
+  
+    // Allocate memory for maximum number of possible keys 
+    // and child pointers 
+    keys = new int[2*t-1]; 
+    C = new NoB *[2*t]; 
+  
+    // Initialize the number of keys as 0 
+    n = 0; 
+} 
+void inserir(int K)
+{
+  
+    // If tree is empty 
+    if (root == NULL) 
+    { 
+        // Allocate memory for root 
+        raiz = new NoB(t, true); 
+        raiz->keys[0] = k;  // Insert key 
+        raiz->n = 1;  // Update number of keys in root 
+    } 
+    else // If tree is not empty 
+    { 
+        // If root is full, then tree grows in height 
+        if (raiz->n == 2*t-1) 
+        { 
+            // Allocate memory for new root 
+            NoB *s = new NoB(t, false); 
+  
+            // Make old root as child of new root 
+            s->C[0] = root; 
+  
+            // Split the old root and move 1 key to the new root 
+            s->splitChild(0, raiz); 
+  
+            // New root has two children now.  Decide which of the 
+            // two children is going to have new key 
+            int i = 0; 
+            if (s->keys[0] < k) 
+                i++; 
+            s->C[i]->insertNonFull(k); 
+  
+            // Change root 
+            raiz = s; 
+        } 
+        else  // If root is not full, call insertNonFull for root 
+            raiz->insertNonFull(k); 
+    } 
+} 
+   
 
 
-
-
-            }
-
-                bool busca(No x){
-
-                    return false
-                }
-                void inserir(No a){
-
-
-
-                }
-                void remove(No y){
-
-
-
-                }
-
-
-        private:
-            No n;
-
-
-        };
-
+};
 
 #endif //SRC_TRABALHO2_ARVOREB_H
